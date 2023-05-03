274a9152167a985384aa10628667a924523cf631
Sergio <sergiolpezdz@gmail.com>
2023-05-03 18:42:05 +0200

    eliminada carpeta backend

--- backend/users/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import routers
-from .api import SocioViewSet
-
-router = routers.DefaultRouter()
-
-router.register('', SocioViewSet, basename='socios')
-
-urlpatterns = router.urls
