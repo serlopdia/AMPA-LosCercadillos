66a0068dc823e6b70742862c4bf1798619e5eaa2
Sergio <sergiolpezdz@gmail.com>
2023-05-03 02:54:25 +0200

    Limpieza

--- backend/users/api.py
+++
@@ -1,8 +0,0 @@
-from .models import Socio
-from rest_framework import viewsets, permissions
-from .serializer import SocioSerializer
-
-class SocioViewSet(viewsets.ModelViewSet):
-    queryset = Socio.objects.all()
-    permission_classes = [permissions.AllowAny]
-    serializer_class = SocioSerializer
--- frontend-AMPA/src/app/global.ts
+++
@@ -1,1 +0,0 @@
-export const API_url: string = "http://127.0.0.1:8000/api";
