b5e8c6a47d6003c979f659db509dfad05afd58c2
Sergio <sergiolpezdz@gmail.com>
2023-05-11 01:37:12 +0200

    feat(backend): Modelos de la app de Shop
    Hechos los modelos de Producto, Pago, Pedido y LineaPedido

--- backendAMPA/shop/models.py
+++ backendAMPA/shop/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.forms import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+from users.models import Socio
 
 # Create your models here.
+
+class EstadoPago(models.TextChoices):
+    PAGADO = "PAGADO", _("Pagado"),
+    PENDIENTE = "PENDIENTE", _("Pendiente"),
+    RECHAZADO = "RECHAZADO", _("Rechazado"),
+
+class EstadoPedido(models.TextChoices):
+    ENTREGADO = "ENTREGADO", _("Entregado"),
+    PREPARACION = "PREPARACION", _("Preparación"),
+    DEVUELTO = "DEVUELTO", _("Devuelto"),
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=64)
+    descripcion = models.CharField(max_length=256)
+    precio_general = models.FloatField()
+    precio_socio = models.FloatField()
+    imagen = models.CharField(max_length=2048)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        app_label="ampa"
+
+class Pago(models.Model):
+    estado = models.CharField(choices=EstadoPago.choices, max_length=64)
+    cantidad = models.IntegerField()
+    socio = models.ForeignKey(Socio, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        app_label="ampa"
+
+    def clean(self):
+        if self.estado not in ["PAGADO", "PENDIENTE", "RECHAZADO"]:
+            raise ValidationError("El valor del campo 'estado' debe ser uno de los siguientes: 'PAGADO', 'PENDIENTE', 'RECHAZADO'")
+
+class Pedido(models.Model):
+    estado = models.CharField(choices=EstadoPedido.choices, max_length=64)
+    pago = models.ForeignKey(Pago, on_delete=models.SET_NULL, null=True)
+    socio = models.ForeignKey(Socio, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        app_label="ampa"
+
+    def clean(self):
+        if self.estado not in ["ENTREGADO", "PREPARACION", "DEVUELTO"]:
+            raise ValidationError("El valor del campo 'estado' debe ser uno de los siguientes: 'ENTREGADO', 'PREPARACION', 'DEVUELTO'")
+
+class LineaPedido(models.Model):
+    producto = models.ForeignKey(Producto, on_delete=models.SET_NULL, null=True)
+    cantidad = models.IntegerField()
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        app_label="ampa"
--- backendAMPA/shop/urls.py
+++ backendAMPA/shop/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from rest_framework import routers
 
+from shop.api import LineaPedidoViewSet, PagoViewSet, PedidoViewSet, ProductoViewSet
+
 router = routers.DefaultRouter()
 
-""" router.register('socios', SocioViewSet, basename='socios')
-router.register('administradores', AdministradorViewSet, basename='administradores') """
+router.register('producto', ProductoViewSet, basename='producto')
+router.register('pago', PagoViewSet, basename='pago')
+router.register('pedido', PedidoViewSet, basename='pedido')
+router.register('lineapedido', LineaPedidoViewSet, basename='lineapedido')
 
 urlpatterns = router.urls
 
